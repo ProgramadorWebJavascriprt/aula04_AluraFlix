44e752d82012305fc6841d053db962e849c4d382
André Luis Kunert <91467055+ProgramadorWebJavascriprt@users.noreply.github.com>
2021-09-28 11:04:57 -0300

    aula04_aluraFlix_revisão

---
+++ aula04aluraflix.js
@@ -0,0 +1,5 @@
+/* Terça-feira,28/09/2021{à amanhã} das 10:20:00 as  h+|-*/
+var listaFilmes = ["https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg", "https://s2.glbimg.com/fCNS_fXPhGRwlpnAaQLzvGYC1y0=/362x536/https://s2.glbimg.com/UPI9xlM9R9O41YRqSO7R3hTNk_s=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/q/V/Avo9ISQQO3grnARi3JCA/2020-914-a-chegada-poster.jpg","https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/91/90/98/20169244.jpg"]
+for (var i= 0; i < listaFilmes.length;i++){
+  document.write("<img src="+ listaFilmes[i] + ">")
+}
